783b170e89bbdee5397e7783158822e8729a230f
vinay.pandya <005vinay@gmail.com>
2015-08-03 12:20:44 +0530

    query string example of nodejs

---
+++ queryStrings.js
@@ -0,0 +1,11 @@
+/**
+ * Created by vinay.pandya on 8/3/2015.
+ */
+var querystring = require('querystring');
+
+//json data to query string
+console.log(querystring.stringify({ foo: 'bar', baz: ['qux', 'quux'], corge: '' }));
+
+//query string to json data
+console.log(querystring.parse('foo=bar&baz=qux&baz=quux&corge'));
+
